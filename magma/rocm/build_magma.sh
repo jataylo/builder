7bbfc5c922f53eb4618b50f789bdd1e5e40e4119
Jack Taylor <jack.taylor@amd.com>
2023-03-13 15:06:57 +0000

    updates

--- magma/rocm/build_magma.sh
+++ magma/rocm/build_magma.sh
@@ -5,7 +5,7 @@ set -e
 bash ./install_conda.sh
 export PATH=/opt/conda/bin:$PATH
 
-PACKAGE_DIR=${PACKAGE_NAME}
+PACKAGE_DIR=$PWD/${PACKAGE_NAME}
 PACKAGE_FILES=package_files
 mkdir ${PACKAGE_DIR}
 cp ${PACKAGE_FILES}/build.sh ${PACKAGE_DIR}/build.sh
@@ -14,5 +14,5 @@ cp ${PACKAGE_FILES}/meta.yaml ${PACKAGE_DIR}/meta.yaml
 conda install -yq conda-build conda-verify
 (
     set -x
-    conda build --output-folder output "${PACKAGE_DIR}"
+    conda build --output-folder "${PACKAGE_DIR}"/output "${PACKAGE_DIR}"
 )
